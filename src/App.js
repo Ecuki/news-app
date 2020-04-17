a74b6fccbf926de6b96bf78971ad9ba78bf584b7
Ecuki <31224521+Ecuki@users.noreply.github.com>
2020-04-17 21:20:41 +0200

    attach API key to a request via the X-API-Key HTTP header

--- src/App.js
+++ src/App.js
@@ -19,9 +19,10 @@ import Article from "./components/Article";
 import MenuBar from "./components/Menu";
 import Logo from "./components/Logo";
 import { initialSelect } from "./utils/constans";
-// import { API_KEY } from "./config";
 
-const API_KEY = process.env.REACT_APP_API_KEY;
+// import { API_KEY } from "./config"; //development
+const API_KEY = process.env.REACT_APP_API_KEY; //production
+
 const paginationStyle = {
   width: "100vw",
   height: 40,
@@ -47,26 +48,28 @@ function getArticlesUrl(sources, activePage) {
     : "language=en&";
   const country = sources.country ? `country=${sources.country}&` : "";
   const query = sources.query ? `q=${sources.query}&` : "";
-  const page = `page=${activePage}&`;
+  const page = `page=${activePage}`;
   if (sources.sources.length === 0) {
     return `${baseUrl}/top-headlines?${category +
       language +
       country +
       query +
-      page}apiKey=${API_KEY}`;
+      page}`;
   } else {
-    return `${baseUrl}/everything?${sourcesString +
-      language +
-      query +
-      page}apiKey=${API_KEY}`;
+    return `${baseUrl}/everything?${sourcesString + language + query + page}`;
   }
 }
 function getAllSourcesUrl() {
-  return `${baseUrl}/sources?apiKey=${API_KEY}`;
+  return `${baseUrl}/sources?`;
 }
 
 const fetchNews = async (url) => {
-  return (await fetch(url)).json();
+  return (await fetch(url, {
+    method: "GET",
+    headers: {
+      "X-Api-Key": API_KEY,
+    },
+  })).json();
 };
 
 function App() {
